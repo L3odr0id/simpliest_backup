0d6c9a336646c004d6d9db38d4fbed8cb54f9ce8
L3odr0id <leon.leonc@yandex.ru>
2024-03-31 13:19:50 +0300

    FIX systemd timer

--- README.md
+++ README.md
@@ -2,7 +2,9 @@
 These utilities will allow you to backup some folder every night and save a specified number of archives.
 
 ## How to use
-1. git clone 
+1. `git clone https://github.com/L3odr0id/simpliest_backup.git`
 2. Set absolute paths to the `config` file
-3. Run `sh save_zip.sh` to chek if the paths exist (and run the backup once)
-4. Run `sh start_systemd_timer.sh` to schedule the `systemd` timer to run the backup at 02:28 AM every day
+3. Run `bash save_zip.sh` to chek if the paths exist (and run the backup once)
+4. Run `sudo bash start_systemd_timer.sh` to schedule the `systemd` timer to run the backup at 02:28 AM once a day
+
+P.S. Check if timer is actually running using `systemctl --user list-timers`
--- start_systemd_timer.sh
+++ start_systemd_timer.sh
@@ -4,17 +4,29 @@
 SCRIPTPATH="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
 
 echo "[+] Create service file"
-# Thanks to https://gist.github.com/hkoba/e05c91ebdd4e2ffd1d79e78e76cf1ff5
-rm -f sched_backup.service
-touch sched_backup.service
-
+# https://gist.github.com/hkoba/e05c91ebdd4e2ffd1d79e78e76cf1ff5
+ServicePath=/etc/systemd/user/sched_backup.service
+rm -f $ServicePath
+touch $ServicePath
 echo "[Unit]
 Description=Scheduled Backup
 
 [Service]
 Type=simple
-ExecStart=${SCRIPTPATH}/save_zip.sh" > sched_backup.service
+ExecStart=${SCRIPTPATH}/save_zip.sh" > $ServicePath
+
+TimerPath=/etc/systemd/user/sched_backup.timer
+rm -f $TimerPath
+touch $TimerPath
+echo "[Unit]
+Description=Backup Scheduling.
+
+[Timer]
+OnCalendar=*-*-* 02:28:00
+
+[Install]
+WantedBy=timers.target" > $TimerPath # https://discussion.fedoraproject.org/t/systemctl-user-enable-unit-timer-does-not-start-after-reboot/73504/2
 
 echo "[+] Start systemd timer"
-sudo systemctl start sched_backup.timer
-sudo systemctl enable sched_backup.timer
+systemctl --user start sched_backup.timer
+systemctl --user enable sched_backup.timer
